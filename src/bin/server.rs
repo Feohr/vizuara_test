49869a48d3d83116494c09eee0fc3581088e20a1
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-07-31 04:51:09 +0530

    Implement custom server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,38 @@
-use std::process::Command;
+#![feature(io_error_more)]
 
-fn main() {
+use tiny_http::{Server, Request, Response};
+use std::io::{Error, ErrorKind};
+use std::fs::File;
+use anyhow::Result;
+
+pub fn serve() -> Result<()> {
     let port = std::fs::read_to_string("SERVE_PORT").unwrap();
-    let _ = Command::new("python3")
-        .args([
-            "-m",
-            "http.server",
-            &format!("{}", port.trim()),
-            "-d",
-            "data/Push_force",
-        ])
-        .spawn()
-        .unwrap();
+    let port = port.trim();
+
+    let Ok(server) = Server::http(&format!("0.0.0.0:{port}")) else {
+        return Err(
+            Error::new(ErrorKind::NetworkUnreachable,
+            format!("Cannot connect to port {port}")).into(),
+        );
+    };
+
+    for request in server.incoming_requests() {
+        if let Some(response) = match_response(&request) {
+            request.respond(response)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn match_response(request: &Request) -> Option<Response<File>> {
+    match dbg!(request.url()) {
+        request => panic!("Cannot process the request: {request}"),
+    }
+
+    None
+}
+
+fn main() -> Result<()> {
+    Ok(serve()?)
 }
