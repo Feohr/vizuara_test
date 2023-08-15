472ddc8abc3d7952102c286f5009f4995e4cb059
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-08-15 21:24:29 +0530

    Change UI behavior and add some extra details to the code.
    - Change the UI theme to `Adwaita`.
    
    - Add better error handling to the `server.rs`.
    
    - Make the game window modular.
    
    - Better handle process killing functionality to properly.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,38 +1,143 @@
 #![feature(io_error_more)]
 
-use tiny_http::{Server, Request, Response};
-use std::io::{Error, ErrorKind};
-use std::fs::File;
 use anyhow::Result;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Error, ErrorKind};
+use std::iter::Iterator;
+use tiny_http::{Header, Request, Response, Server};
 
-pub fn serve() -> Result<()> {
+static RESPONSE: Lazy<HashMap<&'static str, (&'static str, Option<Header>)>> = Lazy::new(|| {
+    HashMap::<&'static str, (&'static str, Option<Header>)>::from([
+        ("/", ("data/{}/index.html", None)),
+        ("/Build/WebGL.data", ("data/{}/Build/WebGL.data", None)),
+        (
+            "/Build/WebGL.framework.js",
+            ("data/{}/Build/WebGL.framework.js", None),
+        ),
+        (
+            "/Build/WebGL.loader.js",
+            ("data/{}/Build/WebGL.loader.js", None),
+        ),
+        (
+            "/Build/WebGL.wasm",
+            (
+                "data/{}/Build/WebGL.wasm",
+                Header::from_bytes(&b"Content-Type"[..], &b"application/wasm"[..])
+                    .unwrap()
+                    .into(),
+            ),
+        ),
+        (
+            "/TemplateData/favicon.ico",
+            ("data/{}/TemplateData/favicon.ico", None),
+        ),
+        (
+            "/TemplateData/fullscreen-button.png",
+            ("data/{}/TemplateData/fullscreen-button.png", None),
+        ),
+        (
+            "/TemplateData/progress-bar-empty-dark.png",
+            ("data/{}/TemplateData/progress-bar-empty-dark.png", None),
+        ),
+        (
+            "/TemplateData/progress-bar-empty-light.png",
+            ("data/{}/TemplateData/progress-bar-empty-light.png", None),
+        ),
+        (
+            "/TemplateData/progress-bar-full-dark.png",
+            ("data/{}/TemplateData/progress-bar-full-dark.png", None),
+        ),
+        (
+            "/TemplateData/progress-bar-full-light.png",
+            ("data/{}/TemplateData/progress-bar-full-light.png", None),
+        ),
+        (
+            "/TemplateData/unity-logo-dark.png",
+            ("data/{}/TemplateData/unity-logo-dark.png", None),
+        ),
+        (
+            "/TemplateData/unity-logo-light.png",
+            ("data/{}/TemplateData/unity-logo-light.png", None),
+        ),
+        (
+            "/TemplateData/webgl-logo.png",
+            ("data/{}/TemplateData/webgl-logo.png", None),
+        ),
+        (
+            "/TemplateData/style.css",
+            ("data/{}/TemplateData/style.css", None),
+        ),
+        (
+            "StreamingAssets",
+            (
+                "data/{}//StreamingAssets/UnityServicesProjectConfiguration.json",
+                Header::from_bytes(&b"Content-Type"[..], &b"application/json"[..])
+                    .unwrap()
+                    .into(),
+            ),
+        ),
+    ])
+});
+
+pub fn serve(name: &str) -> Result<()> {
     let port = std::fs::read_to_string("SERVE_PORT").unwrap();
     let port = port.trim();
 
-    let Ok(server) = Server::http(&format!("0.0.0.0:{port}")) else {
-        return Err(
-            Error::new(ErrorKind::NetworkUnreachable,
-            format!("Cannot connect to port {port}")).into(),
-        );
-    };
-
-    for request in server.incoming_requests() {
-        if let Some(response) = match_response(&request) {
-            request.respond(response)?;
+    match Server::http(&format!("0.0.0.0:{port}")) {
+        Ok(server) => {
+            for request in server.incoming_requests() {
+                if let Some(response) = match_response(name, &request) {
+                    request.respond(response)?;
+                }
+            }
+        }
+        Err(err) => {
+            return Err(Error::new(
+                ErrorKind::NetworkUnreachable,
+                format!("Cannot connect to port {port} due to {err}"),
+            )
+            .into());
         }
     }
 
     Ok(())
 }
 
-fn match_response(request: &Request) -> Option<Response<File>> {
-    match dbg!(request.url()) {
-        request => panic!("Cannot process the request: {request}"),
+fn match_response(name: &str, request: &Request) -> Option<Response<File>> {
+    let request_url = request.url();
+    if let Some((reponse, ref header)) = RESPONSE.get(request_url) {
+        return get_response_with_file(reponse.replace("{}", name).as_ref(), (*header).clone());
     }
+    panic!("Cannot process the request: {request_url}");
+}
 
-    None
+fn get_response_with_file(path: &str, header: Option<Header>) -> Option<Response<File>> {
+    match File::open(path) {
+        Ok(file) => {
+            let mut response = Response::from_file(file);
+            if let Some(header) = header {
+                response.add_header(header);
+            }
+            return Some(response);
+        }
+        Err(err) => {
+            log::error!("Error while opening file: {}", err);
+            return None;
+        }
+    }
 }
 
 fn main() -> Result<()> {
-    Ok(serve()?)
+    let args = std::env::args();
+    if args.len() != 2_usize {
+        return Err(Error::new(
+            ErrorKind::ConnectionRefused,
+            "The server expects exactly one argument".to_string(),
+        )
+        .into());
+    }
+
+    Ok(serve(args.last().unwrap().as_str())?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,51 @@
 #![feature(panic_update_hook)]
 
-
-use once_cell::sync::Lazy;
+use anyhow::{bail, Result};
+use gtk::glib::*;
 use gtk::prelude::*;
 use gtk::*;
+use once_cell::sync::Lazy;
+use std::fs::*;
+use std::process::Child;
 use std::process::Command;
-use gtk::glib::*;
+use std::rc::Rc;
 use webkit2gtk::{SettingsExt, WebContext, WebView, WebViewExt};
 
-static PORT: Lazy<String> = Lazy::new(|| {
-    format!("{}", std::fs::read_to_string("SERVE_PORT").unwrap().trim())
+static PORT: Lazy<String> = Lazy::new(|| match std::fs::read_to_string("SERVE_PORT") {
+    Ok(port) => format!("{}", port.trim()),
+    Err(err) => {
+        panic!("Cannot read SERVE_PORT value due to error: {}", err);
+    }
 });
 
-fn main_layout() -> Box {
+fn create_button(dir_name: String, image_path: String) -> Button {
     // Icon holder
     let image = Image::builder()
-        .file("data/Push_force/push_force.png")
+        .file(&"data/{}".replace("{}", image_path.as_ref()))
         .height_request(10)
         .width_request(10)
         .build();
 
+    let label = dir_name.replace("_", " ");
     // Button holder
     let btn = Button::builder()
         .height_request(10)
         .width_request(10)
-        .label("Start push force")
+        .label(&"Start {}".replace("{}", label.as_ref()))
         .image(&image)
+        .name(dir_name.as_str())
         .build();
 
-    // The main window of the game
-    let window = WeakRef::<Window>::new();
-
     btn.connect_clicked(move |_| {
+        // The main window of the game
+        let window = WeakRef::<Window>::new();
+
         match window.upgrade() {
             // If game_window is not set
             None => {
-                start_server();
+                start_server(dir_name.as_str());
 
-                let window_inner = new_game_window();
+                let window_inner = new_game_window(label.as_ref());
                 window_inner.connect_delete_event(|_, _| {
                     stop_server();
                     Inhibit(false)
@@ -46,59 +54,132 @@ fn main_layout() -> Box {
 
                 window_inner.show_all();
             }
-            _ => {},
+            _ => {}
         }
     });
 
-    let layout = gtk::Box::builder()
-        .child(&btn)
-        .orientation(Orientation::Vertical)
-        .homogeneous(false)
-        .build();
-
-    layout
+    btn
 }
 
-fn create_window(app: &Application) {
+fn main_layout() -> Result<Grid> {
+    let buttons = scan_games()?;
+
+    let layout = gtk::Grid::builder()
+        .orientation(Orientation::Horizontal)
+        .build();
+
+    for button in buttons {
+        layout.add(&button);
+    }
+
+    Ok(layout)
+}
+
+fn scan_games() -> Result<Vec<Button>> {
+    let mut button_vec = Vec::<Button>::new();
+    let dir = read_dir("data")?;
+    for item in dir {
+        let inner_dir = item?;
+        button_vec.push(create_game_button(&inner_dir)?);
+    }
+    Ok(button_vec)
+}
+
+fn create_game_button(dir: &DirEntry) -> Result<Button> {
+    let dir_name = dbg!(dir.file_name());
+    let Ok(name) = dir_name.clone().into_string() else {
+        bail!("The name of the directory {dir_name:?} contains invalid unicode characters");
+    };
+    let image_name = name.clone() + "/icon.png";
+
+    Ok(create_button(name, image_name))
+}
+
+fn create_window(app: &Application) -> Result<()> {
     let window = Window::builder()
         .application(app)
         .title("Vizuara example")
         .build();
 
-    window.add(&main_layout());
-
+    window.add(&main_layout()?);
+    window.connect_delete_event(move |_, _| {
+        stop_server();
+        Inhibit(false)
+    });
     window.maximize();
+
     window.show_all();
+    Ok(())
 }
 
-fn new_game_window() -> Window {
-    let link = dbg!(format!("http://localhost:{}", PORT.to_string()));
+fn new_game_window(name: &str) -> Rc<Window> {
+    let link = format!("http://localhost:{}", PORT.to_string());
+
+    // When you're too bored, uncomment this and run the program.
+    // let link = "https://silentspacemarine.com/".to_string();
 
     let context = WebContext::default().unwrap();
     let webview = WebView::with_context(&context);
     webview.load_uri(link.as_str());
 
     if let Some(settings) = WebViewExt::settings(&webview) {
-        settings.set_enable_media(true);
-        settings.set_enable_webaudio(true);
+        settings.set_enable_webgl(true);
+        settings.set_enable_javascript(true);
+        settings.set_enable_developer_extras(true);
+        settings.set_enable_page_cache(false);
+        settings.set_enable_html5_local_storage(false);
     }
 
-    dbg!(webview.can_show_mime_type("application/wasm"));
+    let titlebar = HeaderBar::builder().title(name).name(name).build();
+    let close_btn = Button::from_icon_name(Some("window-close"), IconSize::Button);
 
-    let window = Window::builder().title("Push force").build();
+    let window = Rc::new(Window::builder()
+        .type_(WindowType::Toplevel)
+        .title(name)
+        .modal(true)
+        .resizable(true)
+        .build());
+
+    let window_ref = Rc::clone(&window);
+    close_btn.connect_clicked(move |_| {
+        window_ref.close();
+    });
+
+    titlebar.pack_end(&close_btn);
+
     window.add(&webview);
+    window.set_titlebar(Some(&titlebar));
     window.maximize();
 
     window
 }
 
 fn stop_server() {
-    let mut cmd = Command::new("./clear_process.sh").spawn().unwrap();
-    cmd.wait().unwrap();
+    let command_wait = |cmd: &mut Child| {
+        if let Err(err) = cmd.wait() {
+            log::error!(
+                "Error while waiting for the cleaning process to execute: {}",
+                err
+            );
+        }
+    };
+    match Command::new("./clear_process.sh").spawn() {
+        Ok(mut cmd) => command_wait(&mut cmd),
+        Err(err) => {
+            log::error!("Failed to run cleaning process script due to: {}", err);
+        }
+    }
 }
 
-fn start_server() {
-    Command::new("target/debug/server").spawn().unwrap();
+fn start_server(name: &str) {
+    log::info!("Starting server");
+    if let Err(err) = Command::new("target/debug/server")
+        .arg(name)
+        .env("RUST_LOG", "info")
+        .spawn()
+    {
+        log::error!("Failed to run the server due to: {}", err);
+    }
 }
 
 fn main() {
@@ -107,16 +188,29 @@ fn main() {
         prev(info);
     });
 
-    // Initialize GTK and create the window
-    gtk::init().expect("Failed to initialize GTK.");
+    env_logger::init();
 
-    // Show the window and start the GTK main loop
+    if let Err(err) = gtk::init() {
+        log::error!("Failed to initialize GTK due to error: {}", err);
+    }
+
+    let Some(settings) = Settings::default() else {
+        log::error!("Failed to get gtk settings");
+        return
+    };
+    gtk::traits::SettingsExt::set_gtk_theme_name(&settings, Some("Adwaita"));
+
     let app = Application::builder()
         .application_id("com.test.vizuara.rehaan")
         .build();
 
-    // Connect activate
-    app.connect_activate(|app| create_window(app));
+    app.connect_activate(|app| {
+        if let Err(err) = create_window(app) {
+            log::error!("Error: {}", err);
+            return;
+        }
+        log::info!("App started successfully");
+    });
 
     app.run();
 }
